60422c5bdaa568656ef83d37c36910c9c43d0745
gjhenrique <me@gjhenrique.com>
2025-12-13 15:43:23 +0100

    bump nixos

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
